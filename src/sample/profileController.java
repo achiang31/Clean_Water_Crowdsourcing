2ddeabe1a80e0efb95c6611a6088f1d95a769958
xuzihan0311 <Alex@lawn-128-61-52-124.lawn.gatech.edu>
2016-10-02 22:44:24 -0400

    Added error message while creating profile

--- src/sample/profileController.java
+++ src/sample/profileController.java
@@ -13,6 +13,7 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by Alex on 10/2/16.
@@ -54,6 +55,8 @@ public class profileController {
 
     private static String username;
 
+    private Date birthday;
+
     @FXML
     private void initialize() {
         Profile profile = WaterApplication.getUsers().get(username).getProfile();
@@ -71,10 +74,10 @@ public class profileController {
 
     @FXML
     private void OKAction(ActionEvent event) throws IOException {
-        try {
+        if (isInputValid()) {
             applicationController.setUsername(username);
             Profile profile = new Profile(firstName.getText(), lastName.getText(), emailAddress.getText(),
-                    ID.getText(), homeAddress.getText(), month.getText(), day.getText(), year.getText());
+                    ID.getText(), homeAddress.getText(), birthday);
             WaterApplication.setProfile(username, profile);
             ((Node) (event.getSource())).getScene().getWindow().hide();
             Parent application = FXMLLoader.load(getClass().getResource("application.fxml"));
@@ -83,8 +86,6 @@ public class profileController {
             stage.setScene(scene);
             stage.setTitle("Water Application");
             stage.show();
-        } catch (Exception e) {
-            errorMessage.setText("Invalid Birthday!");
         }
     }
 
@@ -98,6 +99,39 @@ public class profileController {
         stage.setScene(scene);
         stage.setTitle("Water Application");
         stage.show();
+    }
+
+    private boolean isInputValid() {
+        String errorMessageStr = "";
+        if (firstName.getText() == null || firstName.getText().length() == 0) {
+            errorMessageStr += "Empty first name!\n";
+        } else if (lastName.getText() == null || lastName.getText().length() == 0) {
+            errorMessageStr += "Empty last name!\n";
+        } else if (emailAddress.getText() == null || emailAddress.getText().length() == 0) {
+            errorMessageStr += "Empty email address!\n";
+        } else if (ID.getText() == null || ID.getText().length() == 0) {
+            errorMessageStr += "Empty ID!\n";
+        } else if (homeAddress.getText() == null || homeAddress.getText().length() == 0) {
+            errorMessageStr += "Empty home address!\n";
+        } else if (month.getText() == null || month.getText().length() == 0
+                || day.getText() == null || day.getText().length() == 0
+                || year.getText() == null || year.getText().length() == 0) {
+            errorMessageStr += "Empty birthday!\n";
+        }
+        try {
+            birthday = new Date(Integer.parseInt(year.getText()) - 1900,
+                    Integer.parseInt(month.getText()) - 1, Integer.parseInt(day.getText()));
+        } catch (Exception e) {
+            if (errorMessageStr.length() == 0) {
+                errorMessageStr += "Not a valid birthday!";
+            }
+        }
+        if (errorMessageStr.length() == 0) {
+            return true;
+        } else {
+            errorMessage.setText(errorMessageStr);
+            return false;
+        }
     }
 
     public static void setUsername(String _username) {
