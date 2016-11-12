2d444bc7305ea4b43955595bf321fcc04ba2ae4c
achiang31 <alan.chiang@live.com>
2016-11-12 13:04:26 -0500

    restructured tests

---
+++ test/registrationControllerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+import sample.AccountType;
+import sample.registrationController;
+import static org.junit.Assert.*;
+
+public class registrationControllerTest {
+    @Before
+    public void setUp() {
+    }
+    @Test
+    public void testAccountType() {
+        assertEquals(registrationController.determineAccountType("User"), AccountType.US);
+        assertEquals(registrationController.determineAccountType("Worker"), AccountType.WK);
+        assertEquals(registrationController.determineAccountType("Manager"), AccountType.MN);
+        assertEquals(registrationController.determineAccountType("mm"), AccountType.AD);
+    }
+}
---
+++ test/reportControllerTest.java
@@ -0,0 +1,38 @@
+import org.junit.Before;
+import org.junit.Test;
+import sample.reportController;
+
+import static org.junit.Assert.*;
+
+public class reportControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void validateLatTestTrue() throws Exception {
+        assertTrue(reportController.validateLat("0"));
+        assertTrue(reportController.validateLat("90"));
+        assertTrue(reportController.validateLat("-90"));
+        assertTrue(reportController.validateLat("-45"));
+        assertTrue(reportController.validateLat("45"));
+    }
+    @Test (expected = IllegalArgumentException.class)
+    public void validateLatTestException() throws Exception {
+        reportController.validateLat("");
+        reportController.validateLat(null);
+    }
+    @Test
+    public void validateLatTestFalse() throws Exception {
+        assertFalse(reportController.validateLat("-91"));
+        assertFalse(reportController.validateLat("91"));
+        assertFalse(reportController.validateLat("123456"));
+        assertFalse(reportController.validateLat("-123456"));
+    }
+    @Test
+    public void validateLong() throws Exception {
+
+    }
+
+}
